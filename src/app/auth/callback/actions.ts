7ed307a90d8e206c5c6226f6f255a39f985f6253
Shashwat Mishra <saamac7777@gmail.com>
2024-09-22 12:16:57 +0530

    test

--- src/app/auth/callback/actions.ts
+++ src/app/auth/callback/actions.ts
@@ -4,6 +4,7 @@ import prisma from "@/db/prisma";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 export async function checkAuthStatus() {
+	
 	const { getUser } = getKindeServerSession();
 	const user = await getUser();
 
